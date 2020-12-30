af017a89b748b9668f5f18f1c177ecffd1083fc9
Robert Obi <robobi97@googlemail.com>
2020-12-30 23:57:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,14 +7,8 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-   base_hash = {
-     :railroads => {
-       
-     }
-     
-     
-     
-   }
+   base_hash = 
+   {:railroads => {}{:pieces => "4"}}
   
   
   
